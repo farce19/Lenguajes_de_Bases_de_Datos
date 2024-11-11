864f1c36a5281aafc265fca37cee6c03e67afc06
Fabricio Arce <fabarcsa@gmail.com>
2024-11-10 22:01:11 -0600

    Agregar proyecto Maven al repositorio

---
+++ src/main/java/com/mycompany/lenguajes_de_bases_de_datos/Lenguajes_de_Bases_de_Datos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lenguajes_de_bases_de_datos;
+
+/**
+ *
+ * @author fabar
+ */
+public class Lenguajes_de_Bases_de_Datos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
