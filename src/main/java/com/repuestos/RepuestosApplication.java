dc7fc1fa4ce461456b75f33b67b8f3f725f57ea2
Fabricio Arce <fabarcsa@hotmail.com>
2024-12-18 01:02:47 -0600

    Inicialización del proyecto Spring Boot

---
+++ src/main/java/com/repuestos/RepuestosApplication.java
@@ -0,0 +1,12 @@
+package com.repuestos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepuestosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepuestosApplication.class, args);
+    }
+}
---
+++ src/test/java/com/repuestos/RepuestosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repuestos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepuestosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
